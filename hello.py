e4e64daa46114d601a2f0fbf4fc64b4d4f7a3405
UEHARA Junji <uehara.junji@ntt-tx.co.jp>
2025-02-14 18:18:16 +0900

    add hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from streamlit-article!")
+import streamlit as st
 
+def main():
+    st.markdown("# Hello streamlit!")
+    st.info("OK!", icon=":material/thumb_up:")
 
 if __name__ == "__main__":
     main()
